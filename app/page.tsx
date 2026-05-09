8e157eab14fc669f044fd952a8b945ebedcb33e0
David Van Kriedt <davidvankriedt@icloud.com>
2026-05-09 19:19:59 +1000

    added notes link to home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div className="flex justify-between w-3xs">
       <a href="https://open.spotify.com/artist/5qO2KqO0ypwGgoLP6PrrUe?si=giTAOfsTTtqI8jz0Rnbmzw">music</a>
       <a href="https://github.com/davidvankriedt">github</a>
+      <a href="https://davidvankriedt.github.io/notes/">notes</a>
     </div>
   );
 }
